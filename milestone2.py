6adac3d0d6e6aa889ef3bdcc8a13a5a0138d0497
KRISHNA VAMSI BALLAVARAPU <vishalakula2726@gmail.com>
2023-01-23 13:52:38 +0530

    third commit

--- milestone2.py
+++ milestone2.py
@@ -1,19 +1,35 @@
 # code to read text file
 # Open function to open the file "MyFile1.txt"
 # (same directory) in append mode and
-from shapely.geometry import LineString
+from shapely.geometry import Polygon
 
 
 def convert(list):
     return tuple(list)
 
+def polygonArea(vertices, n):
+ 
+    # Initialize area
+    area = 0.0
+ 
+    # Calculate value of shoelace formula
+    j = n - 1
+    for i in range(0,n):
+        area += (vertices[j][0] + vertices[i][0]) * (vertices[j][1] - vertices[i][1])
+        j = i   # j is previous vertex to i
+     
+ 
+    # Return absolute value
+    return int(abs(area / 2.0))    
 
 def check_polygon_identity(vertices1, vertices2):
-    print(type(vertices1))
-    line1 = LineString(vertices1)
-    line2 = LineString(vertices2)
-    print(line1.equals(line2))
-    return line1.equals(line2)
+    #print(type(vertices1))
+    poly1 = Polygon(vertices1)
+    poly2 = Polygon(vertices2)
+    #print(line1.equals(line2))
+    if(polygonArea(vertices1,len(vertices1))==polygonArea(vertices2,len(vertices2))):
+        return True
+    return poly1.equals(poly2)
 
 with open("D:\KLA\main\Milestone_Input\Milestone 2\POI.txt") as f:
     lines = f.readlines()
@@ -58,14 +74,16 @@ with open("D:\KLA\main\Milestone_Input\Milestone 2\POI.txt") as f:
                         if neg and number != "":
                             # print(int(number))
                             if count < 3:
-                                vertice.append(int(number))
+                                vertice.append(-int(number))
                                 count += 1
                             else:
                                 vertices.append(convert(vertice))
                                 # print(vertices)
                                 count = 1
                                 vertice = []
-                                vertice.append(int(number))
+                                vertice.append(-int(number))
+                                count+=1
+                            neg = False    
                         elif number != "":
                             # print(int(number))
                             if count < 3:
@@ -79,9 +97,7 @@ with open("D:\KLA\main\Milestone_Input\Milestone 2\POI.txt") as f:
                                 vertice.append(int(number))
                                 count += 1
                         number = ""
-                        neg = False
                         digit = False
-                print(vertices)
                 poly_cnt += 1
                 start = False
 vertices_POI = vertices
@@ -91,7 +107,7 @@ with open("output2.txt", "w") as f:
     f.write("libname egdslib\n")
     f.write("units 0.0001 1e-10\n")
     f.write("\n")
-    f.write("bgnstr 1/19/2023 19:25:24 1/19/2023 19:25:24 ")
+    f.write("bgnstr 1/19/2023 19:25:24 1/19/2023 19:25:24\n")
     f.write("strname top\n\n")
 with open("D:\KLA\main\Milestone_Input\Milestone 2\Source.txt") as f:
     lines = f.readlines()
@@ -135,18 +151,19 @@ with open("D:\KLA\main\Milestone_Input\Milestone 2\Source.txt") as f:
                         if neg and number != "":
                             # print(int(number))
                             if count < 3:
-                                vertice.append(int(number))
+                                vertice.append(-int(number))
                                 count += 1
                             else:
                                 vertices.append(convert(vertice))
                                 # print(vertices)
                                 count = 1
                                 vertice = []
-                                vertice.append(int(number))
+                                vertice.append(-int(number))
+                                count+=1
+                            neg = False    
                         elif number != "":
                             # print(int(number))
                             if count < 3:
-                                vertice.append(number)
                                 vertice.append(int(number))
                                 count += 1
                             else:
@@ -157,15 +174,18 @@ with open("D:\KLA\main\Milestone_Input\Milestone 2\Source.txt") as f:
                                 vertice.append(int(number))
                                 count += 1
                         number = ""
-                        neg = False
                         digit = False
-                print(vertices)
+                
                 if(check_polygon_identity(vertices_POI, vertices)):
-                    
+                    print(vertices)
                     with open("output2.txt", "a") as f:
                         f.write("boundary\nlayer 1\ndatatype 0\n")
-                        f.write("xy " + str(no_of_vertices) + " ")
-                        f.write(str(vertices))
-                        f.write("\nendel\n")
+                        f.write("xy "+str(no_of_vertices)+" ")
+                        for vrs in vertices:
+                            for vr in vrs:
+                                f.write(str(vr)+" ")
+                        for vr in vertices[0]:
+                            f.write(str(vr)+" ")
+                        f.write("\nendel\n")    
                 poly_cnt += 1
                 start = False
